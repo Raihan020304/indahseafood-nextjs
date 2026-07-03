074c15f5a2e6ba050dafcebe3141ebaa031266db
Raihan020304 <raihangp020304@gmail.com>
2026-07-03 13:00:14 +0700

    fix: add homepage for Vercel

---
+++ indahseafood/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Indah Seafood 🦐</h1>
+      <p>Website sudah berhasil deploy di Vercel 🚀</p>
+    </div>
+  );
+}
